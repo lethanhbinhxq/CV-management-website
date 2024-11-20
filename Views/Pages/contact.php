0b74776037f3925a22462297721d8ff7a0400d88
lethanhbinhxq <binh.lethanh@hcmut.edu.vn>
2024-11-20 14:45:00 +0700

    Add model + contact page

---
+++ Views/Pages/contact.php
@@ -0,0 +1,33 @@
+<div class="container my-4">
+    <h1 class="my-4 text-center text-uppercase fw-bold text-main-pink">Contact</h1>
+
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <div class="bg-form rounded">
+
+                <form action="" method="post" class="p-5 mb-5" id="login_form">
+
+                    <div class="mb-3">
+                        <label for="fullname" class="form-label">Full name</label>
+                        <input type="text" class="form-control" id="fullname" name="fullname"
+                            placeholder="Enter your full name" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="password"
+                            placeholder="Enter your email" required>
+                    </div>
+
+                    <div class="mb-3">
+                    <label for="message" class="form-label">Message</label>
+                    <textarea class="form-control" id="message" rows="6"></textarea>
+                    </div>
+
+                    <button type="submit" class="mt-2 btn btn-primary" placeholder="Enter your message">Submit</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
