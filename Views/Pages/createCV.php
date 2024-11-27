cfb3c21e1256cddd470bc03fb28fbab910af8444
lethanhbinhxq <binh.lethanh@hcmut.edu.vn>
2024-11-27 08:50:54 +0700

    Fix redirect page Create CV

--- Views/Pages/createCV.php
+++ Views/Pages/createCV.php
@@ -8,7 +8,7 @@ if (session_status() === PHP_SESSION_NONE) {
 if (!isset($_SESSION['user_id'])) {
     // Nếu chưa đăng nhập, hiển thị thông báo và cung cấp các liên kết đăng nhập hoặc đăng ký
     echo '<div class="alert alert-warning text-center" role="alert">';
-    echo 'You are not logged in. Do you want to <a href="Views/Pages/login.php" class="btn btn-primary">Log In</a> / <a href="Views/Pages/signup.php" class="btn btn-success">Sign Up</a>?';
+    echo 'You are not logged in. Do you want to <a href="index.php?page=login" class="btn btn-primary">Log In</a> / <a href="index.php?page=signup" class="btn btn-success">Sign Up</a> ?';
     echo '</div>';
     exit(); // Dừng lại và không cho phép truy cập vào phần còn lại của trang
 }
